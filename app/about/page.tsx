d65cd336c5d06916d78e066c7fdda1b5c5e3423a
isay83 <jisay.2003@gmail.com>
2024-09-02 13:37:25 -0500

    edit metadata per page

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from "next";
+
+export const generateMetadata = (): Metadata => {
+  return {
+    title: "About Us - E-Commerce",
+    description: "Learn more about our fantastic e-commerce platform.",
+  };
+};
+
+export default function page() {
+  return <div>This is my about page</div>;
+}
